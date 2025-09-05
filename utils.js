5e0bff129c48e2c34a1835f0ca5ac93a1e5da628
adverot <ad.verot@gmail.com>
2025-09-05 10:04:13 +0200

    Ajout d'une feature de test dans getStep et setStep

--- utils.js
+++ utils.js
@@ -8,9 +8,10 @@ const dataDir = path.join(process.cwd(), 'data');
  * @param {string} sourceName - Le nom de la source (ex: 'french_fab').
  * @param {string} stepName - Le nom de l'étape (ex: 'urls', 'details').
  * @returns {Array} - Un tableau avec les données de l'étape, ou un tableau vide en cas d'erreur ou si le fichier n'existe pas.
+ * @param {boolean} [test=false] - Indique si on est en phase de test (dataset réduits) ou non
  */
-export function getStep(sourceName, stepName) {
-    const filePath = path.join(dataDir, `${sourceName}-${stepName}.json`);
+export function getStep(sourceName, stepName, test = false) {
+    const filePath = test ? path.join(dataDir, `${sourceName}-${stepName}.test.json`) : path.join(dataDir, `${sourceName}-${stepName}.json`);
 
     try {
         if (fs.existsSync(filePath)) {
@@ -29,9 +30,10 @@ export function getStep(sourceName, stepName) {
  * @param {string} sourceName - Le nom de la source (ex: 'french_fab').
  * @param {string} stepName - Le nom de l'étape (ex: 'urls', 'details').
  * @param {object|Array} data - Les données à sauvegarder.
+ * @param {boolean} [test=false] - Indique si on est en phase de test (dataset réduits) ou non
  */
-export function setStep(sourceName, stepName, data) {
-    const filePath = path.join(dataDir, `${sourceName}-${stepName}.json`);
+export function setStep(sourceName, stepName, data, test = false) {
+    const filePath = test ? path.join(dataDir, `${sourceName}-${stepName}.test.json`) : path.join(dataDir, `${sourceName}-${stepName}.json`);
 
     try {
         // S'assure que le dossier /data existe.
